4f231f16dd685f455f1bda6c3f5214efff4a22e8
Jose Cisneros <jcis494@gmail.com>
2017-04-06 19:17:58 -0500

    Store CRUD finished

--- app/controllers/admin/stores_controller.rb
+++ app/controllers/admin/stores_controller.rb
@@ -5,21 +5,48 @@ class Admin::StoresController < AdminController
   end
 
   def create
+    @store = Store.new(store_params)
+    if @store.save
+      redirect_to admin_stores_path
+    else
+      redirect_to new_admin_store_path
+    end
   end
 
   def new
+    @store = Store.new
   end
 
   def edit
+    @store = Store.find(params[:id])
   end
 
   def show
+    @store = Store.find(params[:id]) # Returns only one object, where returns all. 'params' searches Primary Key
   end
 
   def update
+    @store = Store.find(params[:id])
+    if @store.update(store_params)
+      redirect_to admin_stores_path
+    else
+      redirect_to edit_admin_store_path
+    end
   end
 
   def destroy
+    @store = Store.find(params[:id])
+    if @store.destroy
+      redirect_to admin_stores_path
+    else
+      redirect_to admin_stores_path
+    end
+  end
+
+  protected
+
+  def store_params
+    params.require(:store).permit(:name,:address,:opening_hour,:closing_hour,:phone)
   end
 
 end

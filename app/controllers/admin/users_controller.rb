cc661c7a232d541689d8d0d6000773582cf2edd7
Jose Cisneros <jcis494@gmail.com>
2017-04-24 08:29:23 -0500

    Users Views Completed. With validation for existing users.

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -9,11 +9,16 @@ class Admin::UsersController < AdminController
   end
 
   def create
-    @user = User.new(load_user_params)
-    if @user.save
-      redirect_to admin_users_path
-    else
+    if User.find_by(username: params[:user][:username])
+      flash[:danger] = "Username " + params[:user][:username] + " already in use."
       redirect_to new_admin_user_path
+    else
+      @user = User.new(load_user_params)
+      if @user.save
+        redirect_to admin_users_path
+      else
+        redirect_to new_admin_user_path
+      end
     end
   end
 
@@ -26,11 +31,16 @@ class Admin::UsersController < AdminController
   end
 
   def update
-    @user = User.find(params[:id])
-    if @user.update(load_user_params)
-      redirect_to admin_users_path
-    else
+    if User.where.not(id: params[:id]).find_by(username: params[:user][:username])
+      flash[:danger] = "Username " + params[:user][:username] + " already in use."
       redirect_to edit_admin_user_path
+    else
+      @user = User.find(params[:id])
+      if @user.update(load_user_params)
+        redirect_to admin_users_path
+      else
+        redirect_to edit_admin_user_path
+      end
     end
   end
 
